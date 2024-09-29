bf877160099080f4df986cec58b002e5c793ae4b
AlexandrBxbrxv <g1026@bk.ru>
2024-09-29 05:16:01 +0500

    + модель Client

--- mailings/models.py
+++ mailings/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Message(models.Model):
     topic = models.CharField(max_length=100, verbose_name='тема')
@@ -11,3 +13,16 @@ class Message(models.Model):
     class Meta:
         verbose_name = 'сообщение'
         verbose_name_plural = 'сообщения'
+
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name='email')
+    full_name = models.CharField(max_length=200, verbose_name='фио')
+    comment = models.TextField(**NULLABLE)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
