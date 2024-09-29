26415a8d30567f3039efa143d211d5dc062096db
AlexandrBxbrxv <g1026@bk.ru>
2024-09-29 05:08:41 +0500

    + модель Message

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    topic = models.CharField(max_length=100, verbose_name='тема')
+    body = models.TextField(verbose_name='содержимое')
+
+    def __str__(self):
+        return self.topic
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
