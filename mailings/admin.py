4d4cfc4caf69606f4f55fb09b1ae9d4c0c8a14ff
AlexandrBxbrxv <g1026@bk.ru>
2024-09-29 06:47:16 +0500

    + Отображение моделей Message, Mailing,Client

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailings.models import Message, Mailing, Client
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'body',)
+    search_fields = ('topic',)
+
+
+@admin.register(Mailing)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'first_sending', 'interval', 'status', 'message',)
+    list_filter = ('message',)
+    search_fields = ('name',)
+
+
+@admin.register(Client)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name',)
+    search_fields = ('full_name',)
