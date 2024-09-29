58ee2395e691a82256e8b6e988c16162cb84d3bc
AlexandrBxbrxv <g1026@bk.ru>
2024-09-29 18:38:46 +0500

    Правки PEP8

--- mailings/admin.py
+++ mailings/admin.py
@@ -10,20 +10,20 @@ class MessageAdmin(admin.ModelAdmin):
 
 
 @admin.register(Mailing)
-class MessageAdmin(admin.ModelAdmin):
+class MailingAdmin(admin.ModelAdmin):
     list_display = ('name', 'first_sending', 'interval', 'status', 'message',)
     list_filter = ('message',)
     search_fields = ('name',)
 
 
 @admin.register(Client)
-class MessageAdmin(admin.ModelAdmin):
+class ClientAdmin(admin.ModelAdmin):
     list_display = ('email', 'full_name',)
     search_fields = ('full_name',)
 
 
 @admin.register(MailingTry)
-class MessageAdmin(admin.ModelAdmin):
+class MailingTryAdmin(admin.ModelAdmin):
     list_display = ('try_number', 'last_try', 'try_status', 'try_count',)
     list_filter = ('try_status',)
     search_fields = ('try_number',)
