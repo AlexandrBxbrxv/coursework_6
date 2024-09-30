c24d9ce63b4ccb9fc8433ca8a6ef57ce30586ff5
AlexandrBxbrxv <g1026@bk.ru>
2024-09-30 15:51:04 +0500

    переместил внешний ключ на попытку отправки из модели Mailing в MailingTry

--- mailings/models.py
+++ mailings/models.py
@@ -15,22 +15,6 @@ class Message(models.Model):
         verbose_name_plural = 'сообщения'
 
 
-class MailingTry(models.Model):
-    try_count = models.PositiveIntegerField(default=0, verbose_name='попыток рассылки')
-    last_try = models.DateField(help_text='дата и время последней попытки', verbose_name='последняя попытка')
-    try_status = models.BooleanField(default=False, verbose_name='статус попытки')
-    email_response = models.TextField(**NULLABLE, verbose_name='ответ почтового сервера')
-    try_number = models.CharField(max_length=100, verbose_name='номер попытки')
-
-    def __str__(self):
-        return self.try_number
-
-    class Meta:
-        verbose_name = 'попытка рассылки'
-        verbose_name_plural = 'попытки рассылки'
-        ordering = ('try_number',)
-
-
 class Client(models.Model):
     email = models.EmailField(unique=True, verbose_name='email')
     full_name = models.CharField(max_length=200, verbose_name='фио')
@@ -68,12 +52,29 @@ class Mailing(models.Model):
 
     clients = models.ManyToManyField(Client, related_name='mailing_clients', verbose_name='клиенты рассылки')
 
-    mailing_try = models.ForeignKey(MailingTry, **NULLABLE, on_delete=models.SET_NULL,
-                                    related_name='mailing_tries', verbose_name='попытки рассылки')
-
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = 'рассылка'
         verbose_name_plural = 'рассылки'
+        ordering = ('first_sending',)
+
+
+class MailingTry(models.Model):
+    try_count = models.PositiveIntegerField(default=0, verbose_name='попыток рассылки')
+    last_try = models.DateField(help_text='дата и время последней попытки', verbose_name='последняя попытка')
+    try_status = models.BooleanField(default=False, verbose_name='статус попытки')
+    email_response = models.TextField(**NULLABLE, verbose_name='ответ почтового сервера')
+    try_number = models.CharField(max_length=100, verbose_name='номер попытки')
+
+    mailing = models.ForeignKey(Mailing, **NULLABLE, on_delete=models.SET_NULL,
+                                related_name='tries_mailing', verbose_name='рассылка попытки')
+
+    def __str__(self):
+        return self.try_number
+
+    class Meta:
+        verbose_name = 'попытка рассылки'
+        verbose_name_plural = 'попытки рассылки'
+        ordering = ('try_number', 'last_try',)
