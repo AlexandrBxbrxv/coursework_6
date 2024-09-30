1ad520e41dbcd1cef3d313c6520c69a62b525a95
AlexandrBxbrxv <g1026@bk.ru>
2024-09-30 14:57:48 +0500

    + отображение поля id для всех моделей админки

--- mailings/admin.py
+++ mailings/admin.py
@@ -5,25 +5,25 @@ from mailings.models import Message, Mailing, Client, MailingTry
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('topic', 'body',)
+    list_display = ('id', 'topic', 'body',)
     search_fields = ('topic',)
 
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'first_sending', 'interval', 'status', 'message',)
+    list_display = ('id', 'name', 'first_sending', 'interval', 'status', 'message',)
     list_filter = ('message',)
     search_fields = ('name',)
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('email', 'full_name',)
+    list_display = ('id', 'email', 'full_name',)
     search_fields = ('full_name',)
 
 
 @admin.register(MailingTry)
 class MailingTryAdmin(admin.ModelAdmin):
-    list_display = ('try_number', 'last_try', 'try_status', 'try_count',)
+    list_display = ('id', 'try_number', 'last_try', 'try_status', 'try_count',)
     list_filter = ('try_status',)
     search_fields = ('try_number',)
--- users/admin.py
+++ users/admin.py
@@ -5,5 +5,5 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email',)
+    list_display = ('id', 'email',)
     search_fields = ('full_name',)
