9746f76dc2a9e21e441b2e1b2fb93d46c8b99b85
AlexandrBxbrxv <g1026@bk.ru>
2024-10-03 09:41:03 +0500

    + настройки путей для логин и логаут

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from users.apps import UsersConfig
 
-from users.views import UserRegister, email_verification
+from users.views import UserRegister, email_verification, UserLogin
 
 app_name = UsersConfig.name
 
 
 urlpatterns = [
+    path('login/', UserLogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserRegister.as_view(), name='register'),
     path('email-confirm/<str:token>/', email_verification, name='email_confirm'),
 ]
