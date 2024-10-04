670afd967e200d8cf0fdb9e76fd67161d5ac3c94
AlexandrBxbrxv <g1026@bk.ru>
2024-10-04 17:03:55 +0500

    добавлено поле владелец

--- mailings/admin.py
+++ mailings/admin.py
@@ -5,26 +5,28 @@ from mailings.models import Message, Mailing, Client, MailingTry
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'topic', 'body',)
+    list_display = ('id', 'topic', 'body', 'owner',)
+    list_filter = ('owner',)
     search_fields = ('topic',)
 
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'first_sending', 'interval', 'status', 'message',)
+    list_display = ('id', 'name', 'first_sending', 'interval', 'status', 'message', 'owner',)
     filter_horizontal = ('clients',)
-    list_filter = ('message',)
+    list_filter = ('message', 'owner',)
     search_fields = ('name',)
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'full_name',)
+    list_display = ('id', 'email', 'full_name', 'owner',)
+    list_filter = ('owner',)
     search_fields = ('full_name',)
 
 
 @admin.register(MailingTry)
 class MailingTryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'try_number', 'last_try', 'try_status', 'try_count', 'mailing',)
-    list_filter = ('try_status', 'mailing',)
-    search_fields = ('try_number',)
+    list_display = ('id', 'name', 'last_try', 'try_status', 'mailing', 'owner',)
+    list_filter = ('try_status', 'mailing', 'owner',)
+    search_fields = ('name',)
