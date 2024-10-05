f0dbbd31481793b62a6152381680fe2551dead99
AlexandrBxbrxv <g1026@bk.ru>
2024-10-05 15:22:20 +0500

    + при создании добавляется в группу админ

--- users/management/commands/create_admin.py
+++ users/management/commands/create_admin.py
@@ -1,5 +1,6 @@
 import os
 
+from django.contrib.auth.models import Group
 from dotenv import load_dotenv
 from django.core.management import BaseCommand
 from config.settings import BASE_DIR
@@ -12,8 +13,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
             email=os.getenv('ADMIN_EMAIL'),
-            is_staff=True,
-            is_superuser=True,
         )
         user.set_password(os.getenv('ADMIN_PASSWORD'))
+
+        user_group = Group.objects.get(name='admin')
+        user_group.user_set.add(user)
+
         user.save()
