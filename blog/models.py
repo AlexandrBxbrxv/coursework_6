6252a0855eefe34667aea569bffe35c3bff91448
AlexandrBxbrxv <g1026@bk.ru>
2024-10-09 15:12:32 +0500

    изменение поля owner

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,8 @@ class Blog(models.Model):
     publish_date = models.DateField(auto_now=True, verbose_name='дата публикации')
     is_published = models.BooleanField(default=True, verbose_name='признак публикации')
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_owner', verbose_name='владелец')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE, related_name='blog_owner',
+                              verbose_name='владелец')
 
     def __str__(self):
         return self.title
@@ -21,3 +22,4 @@ class Blog(models.Model):
     class Meta:
         verbose_name = 'блог'
         verbose_name_plural = 'блоги'
+        ordering = ('title', 'views_count',)
