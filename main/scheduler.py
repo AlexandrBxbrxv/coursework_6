55cf7d8308f9514870bc52801ca5cafcf3c96bf6
AlexandrBxbrxv <g1026@bk.ru>
2024-10-11 06:56:00 +0500

    почистил, убрал повторяющийся код

--- main/scheduler.py
+++ main/scheduler.py
@@ -1,8 +1,7 @@
 import logging
-from datetime import datetime
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from django.conf import settings
+from config import settings
 from apscheduler.triggers.cron import CronTrigger
 
 
@@ -10,35 +9,14 @@ logger = logging.getLogger(__name__)
 scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
 
 
-def send_mailing():
-    import pytz
-    from django.core.mail import send_mail
-    from config.settings import EMAIL_HOST_USER
-    from mailings.models import Mailing
-
-    zone = pytz.timezone(settings.TIME_ZONE)
-    current_datetime = datetime.now(zone)
-    # создание объекта с применением фильтра
-    mailings = Mailing.objects.filter(first_sending__lte=current_datetime).filter(
-        status__in=['waiting_first', 'send_waiting'])
-
-    for mailing in mailings:
-        print(mailing.name)
-        send_mail(
-            subject=mailing.message.topic,
-            message=mailing.message.body,
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[client.email for client in mailing.clients.all()],
-        )
-
-
 def run_scheduler():
     from django_apscheduler.jobstores import DjangoJobStore
+    from main.services import send_mailing
 
     scheduler.add_jobstore(DjangoJobStore(), "default")
     scheduler.add_job(
         send_mailing,
-        trigger=CronTrigger(minute="*"),
+        trigger=CronTrigger(second="*/10"),
         id="my_job",
         max_instances=1,
         replace_existing=True,
