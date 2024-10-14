8695bf99e58f0e9b1605d94e43be14c5440e7508
AlexandrBxbrxv <g1026@bk.ru>
2024-10-15 04:28:54 +0500

    кеширование контроллера подробностей Blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
 from blog.views import BlogCreate, BlogList, BlogDetail, BlogUpdate, BlogDelete
@@ -9,7 +10,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('blog/create/', BlogCreate.as_view(), name='blog_create'),
     path('blog/list/', BlogList.as_view(), name='blog_list'),
-    path('blog/detail/<int:pk>/', BlogDetail.as_view(), name='blog_detail'),
+    path('blog/detail/<int:pk>/', cache_page(60)(BlogDetail.as_view()), name='blog_detail'),
     path('blog/update/<int:pk>/', BlogUpdate.as_view(), name='blog_update'),
     path('blog/delete/<int:pk>/', BlogDelete.as_view(), name='blog_delete'),
 ]
